0023c9332f4b758f9f9e20780c7a08cdcfb6ac56
Aravinth Thangavel <arawindth@outlook.com>
2022-09-18 00:16:43 -0500

    option and composition api inital setup

---
+++ src/views/CompositionApiTest.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h2>
+      {{message}}
+    </h2>
+  </section>
+
+</template>
+
+<script lang ="ts">
+  import {ref} from 'vue'
+  export default{
+    setup(){
+      const message = ref("Have a nice day!!!!");
+      return{
+        message
+      }
+    }
+  }
+</script>
---
+++ src/views/OptionsApiTest.vue
@@ -0,0 +1,21 @@
+ 
+
+<template>
+  <section>
+    <div>
+      <h1>{{message}}</h1>
+    </div>
+  </section>
+
+</template>
+
+<script lang = "ts">
+
+export default {
+  data() {
+    return{
+      message:"Hello World!!" 
+    }
+  }
+}
+</script>
